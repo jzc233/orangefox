0a91daf54dd6fd2da40d80647a229aca6be88870
Darth9 <reyprimerjefe@gmail.com>
2023-05-01 13:38:40 +0100

    Update the build vars

--- vendorsetupcommon.sh
+++ vendorsetupcommon.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2018-2022 The OrangeFox Recovery Project
+# 	Copyright (C) 2018-2023 The OrangeFox Recovery Project
 #	
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -41,6 +41,9 @@
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1230768000"; # set build date to Jan 1 2009 00:00:00
 
+	# patch avb20 - some ROM recoveries try to overwrite custom recoveries
+	export OF_PATCH_AVB20=1
+
 	# delta OTA for custom ROMs
         export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
         export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
@@ -56,14 +59,22 @@
 		export OF_QUICK_BACKUP_LIST="/boot;/data;"
 		export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 		export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-		export OF_QUICK_BACKUP_LIST="/boot;/data;"
-		export FOX_VERSION=$FOX_VERSION"_dynamic"
 		export FOX_USE_KEYMASTER_4=1
+		export OF_VANILLA_BUILD=1
+		export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
+		export OF_DISABLE_OTA_MENU=1
+		unset OF_SUPPORT_ALL_BLOCK_OTA_UPDATES
+		unset OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR
+		export FOX_VARIANT="dynamic"
+		#export FOX_VERSION=$FOX_VERSION"_dynamic"
 	else
-		export OF_PATCH_AVB20=1
 		export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
 		if [ "$FOX_USE_KEYMASTER_4" = "1" ]; then
-		   export FOX_VERSION=$FOX_VERSION"_keymaster4"
+		   	export OF_VANILLA_BUILD=1
+		   	export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
+		   	unset OF_SUPPORT_ALL_BLOCK_OTA_UPDATES
+		   	unset OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR
+		   	export FOX_VARIANT="keymaster4"
 		fi
 	fi
 #fi
